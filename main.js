3f9d310d24b8a38e361aeb1493183560b5e6ea46
João Pedro V <mastergamerjp06@gmail.com>
2025-06-24 15:35:54 -0300

    palavras chave

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ const easyMDE = new EasyMDE({
     toolbar: ["bold", "italic", "heading", "|", "quote", "unordered-list", "ordered-list", "|", "link", "image", "|", "preview", "side-by-side", "fullscreen", "|", "guide"],
     sideBySideFullscreen: false
 });
+
+const palavrasChaveHTML = document.querySelector("#palavrasChave")
+
+const tagBox = document.querySelector(".tag-box")
+
+const tags = []
+
+function addKeyword(){
+    const keyword = palavrasChaveHTML.value
+    tags.push(keyword)
+    palavrasChaveHTML.value = ""
+
+    renderTags()
+}
+
+function renderTags(){
+    tagBox.innerHTML = ""
+    for (let tag in tags){
+        tagBox.innerHTML = tagBox.innerHTML + `<span class="tag">${tags[tag]}<i class="bx bx-x" onclick="removeTag(${tag})"></i> </span>`
+    }
+}
+
+function removeTag(tag){
+    tags.splice(tag, 1)
+    renderTags()
+}
